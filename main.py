3fbd7678880ddae1878f9f1a951e34cac6a9c53a
Jessy <jessica.luiz@maisunifacisa.com.br>
2024-12-06 20:45:08 -0300

    Configurando a main para exibir as funcionalidades da biblioteca.py

--- main.py
+++ main.py
@@ -10,41 +10,70 @@ if __name__ == "__main__":
     biblioteca = BibliotecaAvancada()
     comando = 0
 
-    while comando != 6:
-
-        print("           ┏━━━━━━━━━━━━━┓")
+    while comando != 7:
+        print("\n           ┏━━━━━━━━━━━━━┓")
         print("                 MENU     ")
         print("           ┗━━━━━━━━━━━━━┛" + "\n")
         print("[ 1 ] Cadastrar um novo livro")
-        print("[ 2 ] Listar livros")
-        print("[ 3 ] Visualizar temas e livros")
-        print("[ 4 ] Recomendar livros por tema")
-        print("[ 5 ] Remover um livro")
-        print("[ 6 ] Sair do sistema" + "\n")
+        print("[ 2 ] Listar todos os livros")
+        print("[ 3 ] Visualizar detalhes de um livro")
+        print("[ 4 ] Remover um livro")
+        print("[ 5 ] Visualizar livros por tema")
+        print("[ 6 ] Recomendar livros por autor")
+        print("[ 7 ] Sair do sistema\n")
 
-        comando = int(input("▶ Escolha a opção que deseja acessar: "))
+        try:
+            comando = int(input("▶ Escolha a opção que deseja acessar: "))
+        except ValueError:
+            print("Por favor, digite um número válido.")
+            continue
 
         if comando == 1:
-            titulo = input("\n▶ Digite o nome do livro: ")
-            autor = input("▶ Digite o nome do autor do livro: ")
-            ano = int(input("▶ Digite o ano de publicação do livro: "))
+            titulo = input("\n▶ Digite o título do livro: ")
+            autor = input("▶ Digite o autor do livro: ")
+            try:
+                ano = int(input("▶ Digite o ano de publicação do livro: "))
+            except ValueError:
+                print("Por favor, insira um ano válido.")
+                continue
             tema = input("▶ Digite o tema do livro: ")
             livro = Livro(titulo, autor, ano, tema)
-            biblioteca.livros.append(livro)
+            biblioteca.cadastrar_livro(livro, tema)
+            print("Livro cadastrado com sucesso!")
 
         elif comando == 2:
-            biblioteca.exibir_livros()
+            biblioteca.listar_livros()
 
         elif comando == 3:
-            biblioteca.exibir_temas()
+            try:
+                index = int(input("▶ Digite o ID do livro para visualizar os detalhes: ")) - 1
+                biblioteca.visualizar_livro(index)
+            except ValueError:
+                print("Por favor, insira um ID válido.")
+            except IndexError:
+                print("Índice fora do intervalo.")
 
         elif comando == 4:
-            tema = input("▶ Digite o tema para recomendação: ")
-            biblioteca.recomendar_por_tema(tema)
+            try:
+                index = int(input("▶ Digite o ID do livro que deseja remover: ")) - 1
+                biblioteca.remover_livro(index)
+                print("Livro removido com sucesso!")
+            except ValueError:
+                print("Por favor, insira um ID válido.")
+            except IndexError:
+                print("Índice fora do intervalo.")
+
+        elif comando == 5:
+            tema = input("▶ Digite o tema para visualizar os livros: ")
+            biblioteca.visualizar_tema(tema)
 
         elif comando == 6:
-            print("Saindo do sistema.")
+            autor = input("▶ Digite o nome do autor para recomendações: ")
+            biblioteca.recomendar_por_autor(autor)
+
+        elif comando == 7:
+            print("Saindo do sistema. Até mais!")
             break
 
         else:
-            print("Comando inválido.")
+            print("Comando inválido. Tente novamente.")
